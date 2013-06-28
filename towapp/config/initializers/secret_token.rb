c732cc559f33793c1d2e3e2d10ec2498b27cfcd1
mike <michaxze@gmail.com>
2013-06-28 11:29:59 +0800

    new files

---
+++ towapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Towapp::Application.config.secret_token = 'be9f54679d441b4de1f6dd2fb83a570f516041c1d6b36e2d914f494d9932e73d1e3794d0d9b97609ebee36a7640679d87015191f1f67b6817f4966f036a6bd58'
